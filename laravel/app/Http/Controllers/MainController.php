f81dc4f2ba84fa13363ea05cc4085f3ad1c7fd92
Kali <sezerburkas@gmail.com>
2022-07-13 04:41:54 -0400

    construct controller and add blade view

--- laravel/app/Http/Controllers/MainController.php
+++ laravel/app/Http/Controllers/MainController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-  // 
+    
+    /**
+     * Create a new controller instance
+     *
+     * @return void
+     */
+    public function __construct(){
+        
+        $this->middleware('auth');
+    }
+
+    public function myAccount(){
+        return view('account.index');
+    }
 }
