dc5ae48eec120495b7450d505fe56f8afa4ac7b1
Kali <sezerburkas@gmail.com>
2022-07-17 12:07:55 -0400

    install: sanctum

--- laravel/vendor/composer/installed.php
+++ laravel/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'fa670168121e8a66b771ca33e10f4987f5318987',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -374,9 +374,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'fa670168121e8a66b771ca33e10f4987f5318987',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
