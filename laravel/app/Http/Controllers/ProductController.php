e519b9da369151499f55432e2088b2166cbe26a1
Kali <sezerburkas@gmail.com>
2022-07-17 12:05:25 -0400

    building the api

--- laravel/app/Http/Controllers/ProductController.php
+++ laravel/app/Http/Controllers/ProductController.php
@@ -2,9 +2,105 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:50',
+            'short_desc' => 'required|max:200',
+            'long_desc' => 'max:1000',
+            'price' => 'required|numeric',
+            'discount' =>'numeric',
+            'stock' => 'numeric',
+            'img' => 'required|max:200',
+        ]);
+
+        return Product::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return Product::find($product);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+
+        $request->validate([
+            'name' => 'max:50',
+            'short_desc' => 'max:200',
+            'price' => 'numeric',
+            'discount' =>'numeric',
+            'stock' => 'numeric',
+            'img' => 'max:200',
+        ]);
+
+        return ($product->update($request->all())>=1)? ['success'=>true,'post'=>$product]:['success'=>false];
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        return $product->delete();
+    }
 }
