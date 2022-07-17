0f0e139ce2d9463a8f5ff3bac7cf9f15cf28f359
Kali <sezerburkas@gmail.com>
2022-07-17 12:20:59 -0400

    add: auth to resources

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -21,6 +21,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resources([
-    'products' => ProductController::class
-]);
+
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+
+    Route::resources([
+        'products' => ProductController::class
+    ]);
+
+});
+
+//Override to make public
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{product}', [ProductController::class, 'show']);
